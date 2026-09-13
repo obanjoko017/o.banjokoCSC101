b39aac18ddd35b22df6ad906bab99c1129345ba2
pluto <Obaloluwa.banjoko@pau.edu.ng>
2026-09-13 13:40:25 -0700

    practice 3

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    Let result = 10.00;      //f64 by default
+    let interest:f32 = 8.35;
+    Let cost:f64 = 15000.600; // double precision
+
+    printin!("result value is {}", result);
+    printin!("interest is {}", interest);
+    printin!("cost is {}", cost);
+}
